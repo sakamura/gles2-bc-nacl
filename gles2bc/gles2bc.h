6dba26685e044726bbdd0518ce5f3482437a0fbb
sakamura <michel@micheldonais.com>
2016-08-26 21:54:52 -0400

    Now compiles. Added Framework project.

---
+++ gles2bc/gles2bc.h
@@ -0,0 +1,28 @@
+//
+//  gles2-bc.h
+//  gles2-bc
+//
+//  Created by Michel Donais on 16-08-18.
+//  Copyright © 2016 Michel Donais. All rights reserved.
+//
+
+#import <UIKit.h>
+
+//! Project version number for gles2-bc.
+FOUNDATION_EXPORT double gles2bcVersionNumber;
+
+//! Project version string for gles2-bc.
+FOUNDATION_EXPORT const unsigned char gles2bcVersionString[];
+
+#import <gles2bc/OpenGLESConfig.h>
+#import <gles2bc/OpenGLESContext.h>
+#import <gles2bc/OpenGLESFile.h>
+#import <gles2bc/OpenGLESImplementation.h>
+#import <gles2bc/OpenGLESMath.h>
+#import <gles2bc/OpenGLESString.h>
+#import <gles2bc/OpenGLESUtil.h>
+#import <gles2bc/OpenGLES11/OpenGLES11Context.h>
+#import <gles2bc/OpenGLES11/OpenGLES11Implementation.h>
+#import <gles2bc/OpenGLES20/OpenGLES20Context.h>
+#import <gles2bc/OpenGLES20/OpenGLES20Implementation.h>
+#import <gles2bc/OpenGLES20/OpenGLESState.h>
